bff8c6c815b198acab1edb04baad1cff97b39421
r-yamada1998 <yamadier@icloud.com>
2021-12-14 20:02:02 +0900

    missing '(' added

--- pycprops/pycprops.py
+++ pycprops/pycprops.py
@@ -13,43 +13,38 @@ from .decomposition import cube_decomp
 import warnings
 import os
 
-#Shut up, I know what I'm doing
+# Shut up, I know what I'm doing
 warnings.simplefilter("ignore")
-np.seterr(all='ignore')
+np.seterr(all="ignore")
 
-def fits2props(cube_file,
-               datadir=os.getcwd(),
-               output_directory=os.getcwd(),
-               mask_file=None,
-               noise_file=None,
-               distance=None,
-               asgnname=None,
-               propsname=None,
-               delta=None,
-               verbose=True,
-               alphaCO=6.7,
-               channelcorr=0.0,
-               allow_huge=False,
-               asgn=None, **kwargs):
 
-    if asgnname is None:
-        asgnname = cube_file.replace('.fits', '_asgn.fits')
-    if propsname is None:
-        propsname = cube_file.replace('.fits', '_props.fits')
+def fits2props(
+    cube_hdu,
+    mask_hdu,
+    noise_hdu=None,
+    distance=None,
+    delta=None,
+    verbose=True,
+    alphaCO=6.7,
+    channelcorr=0.0,
+    allow_huge=False,
+    asgn=None,
+    **kwargs
+):
 
-    s = SpectralCube.read(datadir + '/' + cube_file)
-    mask = fits.getdata(datadir + '/' + mask_file)
+    s = SpectralCube.read(cube_hdu)
+    mask = mask_hdu.data
 
     if allow_huge:
-      s.allow_huge_operations = True
+        s.allow_huge_operations = True
 
-    if noise_file is None:
+    if noise_hdu is None:
         print("No noise file found.  Calculating noise from Med. Abs. Dev of Data")
         noise = s.mad_std().value
         if delta is None:
             delta = 2 * float(noise)
     else:
-        noise = SpectralCube.read(datadir + '/' +noise_file)
+        noise = SpectralCube.read(noise_hdu)
         if delta is None:
             delta = 2 * noise.median().value
 
@@ -63,16 +58,17 @@ def fits2props(cube_file,
     s = s.with_mask(mask)
 
     if asgn is None:
-        asgn = cube_decomp(s, delta=delta, verbose=verbose,
-                           **kwargs)
-        asgn.writeto(output_directory + '/' + asgnname, overwrite=True)
+        asgn = cube_decomp(s, delta=delta, verbose=verbose, **kwargs)
 
-    props = cloudalyze(s, asgn.data,
-                       distance=distance,
-                       verbose=verbose,
-                       noise=noise,
-                       alphaCO=alphaCO,
-                       channelcorr=channelcorr,
-                       **kwargs)
+    props = cloudalyze(
+        s,
+        asgn.data,
+        distance=distance,
+        verbose=verbose,
+        noise=noise,
+        alphaCO=alphaCO,
+        channelcorr=channelcorr,
+        **kwargs
+    )
 
-    props.write(output_directory + '/' + propsname, overwrite=True)
+    return asgn, props
